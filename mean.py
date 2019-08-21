8430e61ec11b652a46784463c4ac1361a5b5e72e
Emily Smith <easmith5@uchicago.edu>
2019-08-21 11:24:30 -0700

    Adding mean files

---
+++ mean.py
@@ -0,0 +1,19 @@
+#from numpy.testing import assert_allclose
+#assert_allclose(obs, exp, atol=err)
+
+def mean(num_list):
+    #assert type(num_list) == list
+    #assert isinstance(num_list, list)
+    try:
+        return sum(num_list)/len(num_list)
+    except ZeroDivisionError:
+        #msg = "Silly, your list is empty."
+        #raise ZeroDivisionError(detail.__str__() + "\n" + msg)
+        return 0
+    except TypeError as detail:
+        msg = "Not a list you dork."
+        raise TypeError(detail.__str__() + "\n" + msg)
+        
+    #if(len(num_list) ==0):
+    #    raise Exception("You suck, why would you give me an empty list.")
+    
